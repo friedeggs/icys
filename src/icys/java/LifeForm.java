3c29ebaff2dc5f194a0ab15329dcc0eb2bbb976d
truffulatree <cc.forgetmenot@gmail.com>
2014-01-15 08:35:09 -0500

    Create LifeForm.java

---
+++ src/icys/java/LifeForm.java
@@ -0,0 +1,25 @@
+import java.awt.image.BufferedImage;
+
+public class LifeForm {
+
+	boolean alive;
+	int x, y;
+	BufferedImage image;
+	
+	public void checkAlive()
+	{
+	}
+	
+	public void move()
+	{
+	}
+	
+	public void show()
+	{	
+	}
+	
+}
+
+
+
+
