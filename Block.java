fa89c1f28062e525de4f770b023c575225bacef3
truffulatree <cc.forgetmenot@gmail.com>
2014-01-15 08:51:03 -0500

    Create Block.java

---
+++ Block.java
@@ -0,0 +1,14 @@
+public class Block
+{
+
+public boolean isOccupied;
+public int value; 
+public int x, y;
+
+  public Block (int value)
+  {
+    boolean isOccupied = false; 
+    this.value = value; 
+  }
+  
+}
