7b8b7e1412ee8f35b62f57ae373e726f2a374eda
Lili Deng <Ever.Blue_11@hotmail.com>
2014-01-20 00:28:21 -0500

    Update Block.java

--- src/icys/java/Block.java
+++ src/icys/java/Block.java
@@ -1,26 +1,39 @@
 package icys.java;
+import java.awt.Color; 
+import java.awt.Graphics;
 
 public class Block
 {
 
 public boolean isOccupied;
 public int value; 
-public int x, y;
+public int row, column,increaseX, increaseY, x /*=768, 567*/, y;
 
-  public Block (int value)
+  public Block (int value, int i, int j)
   {
     boolean isOccupied = false; 
     this.value = value; 
+    row = i;
+    column = j;
   }
   
-  public void set (int x, int y) {
-	  this.x = x;
-	  this.y = y;
-  }
+  /* public void set (int iC, int jC) {
+	  row = iC;
+	  column = jC;
+  }*/
   
-  //public draw()
+  public void draw(Graphics g, char a)
   {
-    //i know what you mean
-    // EDITTT
+  	Color water = new Color (175, 217, 255),
+  	land = new Color (0,0,0),
+  	dark = new Color (11,23,59),
+  	use;
+  	
+  	if (a.equals'x')
+  		use = dark;
+  	else
+  		use = water;
+  	
+  	int [] xPoint = {row*x; row*x+increaseX};
+  	int [] yPoint = {column*y, colume*y+increaseY};
   }
-}
