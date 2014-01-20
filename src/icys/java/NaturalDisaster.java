0db1d14679f51569884d902c921dd4567b773735
AutumnxLavender <Ever.Blue_11@hotmail.com>
2014-01-20 09:21:00 -0500

    'no'

--- src/icys/java/NaturalDisaster.java
+++ src/icys/java/NaturalDisaster.java
@@ -1,18 +1,13 @@
-package icys.java;
+import java.util.ArrayList;
 
-public abstract class NaturalDisaster extends Mode
-{
-}
+public abstract class NaturalDisaster extends Mode {
+}// End of Class
 
-/* ------- Ideas -------
-    ArrayList --> Objects
-    loseBear, loseFish, etc. goes into the program
-    and deletes object from Array[List]
-    meltArea; blackenWater etc. --> GUI?
-    
-    Blocks are the box is the number of box with the certain
-    [ROW NO.] [COL NO.]
-    -1 = Unused
-    0 = Water
-    1 = Land
-   ------- Ideas ------- */
+/*
+ * ------- Ideas ------- ArrayList --> Objects loseBear, loseFish, etc. goes
+ * into the program and deletes object from Array[List] meltArea; blackenWater
+ * etc. --> GUI?
+ * 
+ * Blocks are the box is the number of box with the certain [ROW NO.] [COL NO.]
+ * -1 = Unused 0 = Water 1 = Land ------- Ideas -------
+ */
